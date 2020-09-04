76892ef87c455fc87ab91440779cee4753e8f5eb
ZaveDev <66022270+ZaveDev@users.noreply.github.com>
2020-09-04 13:43:25 -0400

    MVP + Stretch

---
+++ actions/actionsRouter.js
@@ -0,0 +1,86 @@
+const express = require('express');
+
+const actionDb = require('../data/helpers/actionModel')
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  actionDb.get()
+    .then(rez => {
+      res.status(200).json({ data: rez })
+    })
+    .catch(err => {
+      res.status(500).json({
+        errorMessage: "something went wrong",
+        err: err
+      })
+    })
+});
+
+router.post('/:id', (req, res) => {
+  req.body.project_id = req.params.id
+  if ( req.body.description && req.body.notes){
+    actionDb.insert(req.body)
+    .then(rez => {
+      res.status(200).json({ data: rez })
+    })
+    .catch(err => {
+      res.status(500).json({
+        errorMessage: "something went wrong",
+        err: err
+      })
+    })
+  } else {
+    res.status(400).json({message: "project_id, description, or notes was missing from your request"})
+  }
+
+});
+
+router.get('/:id', validateActionId, (req, res) => {
+  res.status(200).json({ data: req.action  })
+});
+
+router.put('/:id', validateActionId, (req, res) => {
+  actionDb.update(req.action.id, req.body)
+    .then(rez => {
+      res.status(200).json({ data: rez }) 
+    })
+    .catch(err => {
+      res.status(500).json({
+        errorMessage: "something went wrong",
+        err: err
+      })
+    })
+});
+
+router.delete('/:id', validateActionId, (req, res) => {
+  actionDb.remove(req.action.id)
+    .then(rez => {
+      console.log(rez);
+      res.status(200).json({ DeleteMessage: `action ${req.action.id} was deleted`}) 
+    })
+    .catch(err => {
+      res.status(500).json({
+        errorMessage: "something went wrong",
+        err: err
+      })
+    })
+});
+
+function validateActionId(req, res, next) {
+  const id = req.params.id
+  actionDb.get(id)
+    .then(rez => {
+      if (rez) {
+        req.action = rez
+        next(); 
+      } else {
+        res.status(400).json({ message: "action with that id was not not found" })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({err: err})
+    })
+}
+
+module.exports = router;
---
+++ projects/projectsRouter.js
@@ -0,0 +1,85 @@
+const express = require('express');
+
+const projectDb = require('../data/helpers/projectModel')
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  projectDb.get()
+    .then(rez => {
+      res.status(200).json({ data: rez })
+    })
+    .catch(err => {
+      res.status(500).json({
+        errorMessage: "something went wrong",
+        err: err
+      })
+    })
+});
+
+router.post('/', (req, res) => {
+  if (req.body.name && req.body.description ){
+    projectDb.insert(req.body)
+    .then(rez => {
+      res.status(200).json({ data: rez }) 
+    })
+    .catch(err => {
+      res.status(500).json({
+        errorMessage: "something went wrong",
+        err: err
+      })
+    })
+  } else {
+    res.status(400).json({message: "name or description was missing from your request"})
+  }
+  
+});
+
+router.get('/:id', validateProjectId, (req, res) => {
+  res.status(200).json({ data: req.project })
+});
+
+router.put('/:id', validateProjectId, (req, res) => {
+  projectDb.update(req.project.id, req.body)
+    .then(rez => {
+      res.status(200).json({ data: rez }) 
+    })
+    .catch(err => {
+      res.status(500).json({
+        errorMessage: "something went wrong",
+        err: err
+      })
+    })
+});
+
+router.delete('/:id', validateProjectId, (req, res) => {
+  projectDb.remove(req.project.id)
+    .then(rez => {
+      console.log(rez);
+      res.status(200).json({ DeleteMessage: `project ${req.project.id} was deleted`})  
+    })
+    .catch(err => {
+      res.status(500).json({
+        errorMessage: "something went wrong",
+        err: err
+      })
+    })
+});
+
+function validateProjectId(req, res, next) {
+  const id = req.params.id
+  projectDb.get(id)
+    .then(rez => {
+      if (rez) {
+        req.project = rez
+        next(); 
+      } else {
+        res.status(400).json({ message: "project with that id was not not found" })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({err: err})
+    })
+}
+
+module.exports = router;
